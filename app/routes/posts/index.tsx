b66858bfe7a939000a234d41325b160c2ed9cf3e
Luke Davies <l.davies@wisr.com.au>
2023-01-26 21:54:31 +0700

    create adminUser hook to hide FE links

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,7 @@
 import { Link, useLoaderData } from "@remix-run/react";
 import { json, LoaderFunction } from "@remix-run/node";
 import { getPostListings } from "~/models/post.server";
-import { useOptionalUser } from "~/utils";
+import { useOptionalAdminUser, useOptionalUser } from "~/utils";
 
 type LoaderData = {
   posts: Awaited<ReturnType<typeof getPostListings>>;
@@ -15,15 +15,17 @@ export const loader: LoaderFunction = async () => {
 
 export default function PostsRoute() {
   const { posts } = useLoaderData() as unknown as LoaderData;
-  const user = useOptionalUser();
-
-  console.log({ user });
+  const adminUser = useOptionalAdminUser();
+  console.log({ adminUser });
   return (
     <main>
       <h1 className="mb-16">Posts</h1>
-      <Link to="/posts/admin" className="text-red-600 underline">
-        Admin
-      </Link>
+      {adminUser && (
+        <Link to="/posts/admin" className="text-red-600 underline">
+          Admin
+        </Link>
+      )}
+
       <ul>
         {posts.map((post) => (
           <li key={post.slug} className="mb-8">
