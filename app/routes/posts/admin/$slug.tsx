725e066b04f83d9030241a5fcaad0253c2d3562b
Luke Davies <l.davies@wisr.com.au>
2023-01-26 22:29:46 +0700

    edit posts

--- app/routes/posts/admin/$slug.tsx
+++ app/routes/posts/admin/$slug.tsx
@@ -4,15 +4,25 @@ import {
   LoaderFunction,
   redirect,
 } from "@remix-run/node";
-import { Form, useActionData, useTransition } from "@remix-run/react";
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useTransition,
+} from "@remix-run/react";
 import invariant from "tiny-invariant";
 
-import { createPost } from "~/models/post.server";
+import { createPost, getPost, updatePost } from "~/models/post.server";
 import { requireAdminUser } from "~/session.server";
 
-export const loader: LoaderFunction = async ({ request }) => {
+export const loader: LoaderFunction = async ({ request, params }) => {
   await requireAdminUser(request);
-  return json({});
+  if (params.slug === "new") {
+    return json({});
+  }
+
+  const post = await getPost(params.slug);
+  return json({ post: post });
 };
 
 type ActionData =
@@ -23,7 +33,7 @@ type ActionData =
     }
   | undefined;
 
-export const action: ActionFunction = async ({ request }) => {
+export const action: ActionFunction = async ({ request, params }) => {
   await requireAdminUser(request);
   const data = await request.formData();
   const title = data.get("title");
@@ -46,19 +56,26 @@ export const action: ActionFunction = async ({ request }) => {
   invariant(typeof slug === "string", "slug must be a string");
   invariant(typeof markdown === "string", "markdown must be a string");
 
-  await createPost({ title, slug, markdown });
+  if (params.slug === "new") {
+    await createPost({ title, slug, markdown });
+  } else {
+    await updatePost(params.slug, { title, slug, markdown });
+  }
 
   return redirect("/posts/admin");
 };
 
 export default function NewPostRoute() {
+  const data = useLoaderData();
   const transition = useTransition();
-  const isCreating = Boolean(transition.submission);
+  const isCreating = transition.submission?.formData.get("intent") === "create";
+  const isUpdating = transition.submission?.formData.get("intent") === "update";
   const inputClassName = "w-full rounded border border-gray-500 px-2 py-2";
   const errors = useActionData() as ActionData;
+  const isNewPost = !data.post;
 
   return (
-    <Form method="post">
+    <Form method="post" key={data.post?.slug ?? "new"}>
       <p>
         <label htmlFor="title">
           Post Title:
@@ -66,7 +83,13 @@ export default function NewPostRoute() {
             <em className="text-red-600">{errors.title}</em>
           ) : null}
         </label>
-        <input type="text" name="title" id="" className={inputClassName} />
+        <input
+          type="text"
+          name="title"
+          id=""
+          className={inputClassName}
+          defaultValue={data.post?.title}
+        />
       </p>
       <p>
         <label htmlFor="slug">
@@ -75,7 +98,13 @@ export default function NewPostRoute() {
             <em className="text-red-600">{errors.slug}</em>
           ) : null}
         </label>
-        <input type="text" name="slug" id="" className={inputClassName} />
+        <input
+          type="text"
+          name="slug"
+          id=""
+          className={inputClassName}
+          defaultValue={data.post?.slug}
+        />
       </p>
       <p>
         <label htmlFor="markdown">
@@ -89,15 +118,19 @@ export default function NewPostRoute() {
           id="markdown"
           rows={20}
           className={`${inputClassName} font-mono`}
+          defaultValue={data.post?.markdown}
         ></textarea>
       </p>
       <p className="text-right">
         <button
           type="submit"
-          disabled={isCreating}
+          name="intent"
+          value={isNewPost ? "create" : "update"}
+          disabled={isCreating || isUpdating}
           className="bg-blue-500 py-2 px-4 text-white"
         >
-          {isCreating ? "submitting" : "submit"}
+          {isNewPost ? (isCreating ? "Creating..." : "Create") : null}
+          {isNewPost ? null : isUpdating ? "Updating..." : "Update"}
         </button>
       </p>
     </Form>
