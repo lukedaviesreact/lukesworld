6188caea59decd2734b2da731a15076b28f5c6a1
Luke Davies <l.davies@wisr.com.au>
2023-03-18 18:08:51 +0700

    remove invariant checks

--- app/env.server.ts
+++ app/env.server.ts
@@ -1,18 +1,4 @@
-import invariant from 'tiny-invariant';
-
 export function getEnv() {
-    invariant(process.env.ADMIN_EMAIL, 'ADMIN_EMAIL should be defined');
-    invariant(process.env.NOTION_KEY, 'NOTION_KEY should be defined');
-    invariant(
-        process.env.NOTION_LUKESWORLD_ID,
-        'NOTION_LUKESWORLD_ID should be defined'
-    );
-
-    invariant(
-        process.env.NOTION_POSTLIST_ID,
-        'NOTION_POSTLIST_ID should be defined'
-    );
-
     return {
         ADMIN_EMAIL: process.env.ADMIN_EMAIL,
         NOTION_KEY: process.env.NOTION_KEY,
