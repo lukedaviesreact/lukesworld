d7e5e0c29540b38b2dd4e0a43f95d69cd8ec27e0
Luke Davies <l.davies@wisr.com.au>
2023-03-23 22:20:01 +0700

    adds favicon

--- app/root.tsx
+++ app/root.tsx
@@ -39,28 +39,28 @@ export const links: LinksFunction = () => {
         {
             rel: 'apple-touch-icon',
             sizes: '180x180',
-            href: 'assets/favicon/apple-touch-icon.png',
+            href: './apple-touch-icon.png',
         },
         {
             rel: 'icon',
             type: 'image/png',
             sizes: '32x32',
-            href: 'assets/favicon/favicon-32x32.png',
+            href: './favicon-32x32.png',
         },
         {
             rel: 'icon',
             type: 'image/png',
             sizes: '16x16',
-            href: 'assets/favicon/favicon-16x16.png',
+            href: './favicon-16x16.png',
         },
         {
             rel: 'mask-icon',
             color: '#5bbad5',
-            href: 'assets/favicon/safari-pinned-tab.svg',
+            href: './safari-pinned-tab.svg',
         },
         {
             rel: 'manifest',
-            href: 'assets/favicon/site.webmanifest',
+            href: './site.webmanifest',
         },
     ];
 };
