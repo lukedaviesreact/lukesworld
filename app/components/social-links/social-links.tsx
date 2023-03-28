cca700f0b86dce15b1d55a52fd6f2bf4e0ab651a
Luke Davies <l.davies@wisr.com.au>
2023-03-28 20:31:22 +0700

    adds resume download

--- app/components/social-links/social-links.tsx
+++ app/components/social-links/social-links.tsx
@@ -1,15 +1,36 @@
-import { HStack, Link } from '@chakra-ui/react';
+import { HStack, Link, Tooltip } from '@chakra-ui/react';
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import { RiSave2Fill } from 'react-icons/ri';
+import * as gtag from '~/utils/gtags.client';
 
 export const SocialLinks = () => {
     return (
         <HStack spacing={2} mt={2}>
-            <Link href="https://www.github.com/lukedxvxes">
-                <AiFillGithub size="24" color="#2d3748" />
-            </Link>
-            <Link href="https://www.linkedin.com/in/luke-davies-3503aa13a/">
-                <AiFillLinkedin size="24" color="#2d3748" />
-            </Link>
+            <Tooltip label="Github">
+                <Link href="https://www.github.com/lukedxvxes">
+                    <AiFillGithub size="24" color="#2d3748" />
+                </Link>
+            </Tooltip>
+            <Tooltip label="Linkedin">
+                <Link href="https://www.linkedin.com/in/luke-davies-3503aa13a/">
+                    <AiFillLinkedin size="24" color="#2d3748" />
+                </Link>
+            </Tooltip>
+            <Tooltip label="Download resume">
+                <Link
+                    href="/luke-davies-front-end-engineer-resume.pdf"
+                    download={true}
+                    onClick={() => {
+                        gtag.event({
+                            action: 'download_resume',
+                            category: 'job-seeking',
+                            label: 'resume downloaded',
+                        });
+                    }}
+                >
+                    <RiSave2Fill size="24" color="#2d3748" />
+                </Link>
+            </Tooltip>
         </HStack>
     );
 };
