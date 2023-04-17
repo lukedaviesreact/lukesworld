1df4b07ff696cb13429a16bc332a51b7af89d35b
lukedxvxes <lukedaviesweb+1@gmail.com>
2023-04-17 15:36:11 +0700

    adds new resume

--- app/components/social-links/social-links.tsx
+++ app/components/social-links/social-links.tsx
@@ -18,7 +18,7 @@ export const SocialLinks = () => {
             </Tooltip>
             <Tooltip label="Download resume">
                 <Link
-                    href="/luke-davies-software-engineer-react.pdf"
+                    href="/luke-davies-software-engineer.pdf"
                     download={true}
                     onClick={() => {
                         gtag.event({
